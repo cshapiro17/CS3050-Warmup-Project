6321c3b57fea7baee8cea0b3f191a1669a2977d4
jamesbush <jjbush@uvm.edu>
2024-02-05 15:39:12 -0500

    Uploaded json to firebase

---
+++ Firebase.py
@@ -0,0 +1,25 @@
+import firebase_admin # if gettting error run in cmd: pip3 install firebase_admin
+from firebase_admin import firestore, credentials
+
+class Firebase:
+
+    
+
+    def __init__(self) -> None:
+        # Add credentials to make connection
+        cred = credentials.Certificate("smithathletes-d8741-firebase-adminsdk-snc4k-3e47fd0006.json")
+        firebase_admin.initialize_app(cred, {"databaseURL": "https://smithathletes-d8741-default-rtdb.firebaseio.com/"})
+
+        # Works on SmithAthlete collection, can change later if collections are added
+        self.db = firestore.client()
+        self.collection_ref = self.db.collection('SmithAthletes')
+
+    def connect(self):
+        pass
+        
+    def retrieveAllData(self):
+        # document_data = collection_ref.document('your_document_id').get().to_dict()
+        print(self.collection_ref.get())
+
+
+
