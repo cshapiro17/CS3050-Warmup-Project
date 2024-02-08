3e75844a687359ab67a3d54b8080fe007ae8eb27
jwmahone <jwmahone@uvm.edu>
2024-02-07 23:12:28 -0500

    worked on parser function

--- parser_1.py
+++ parser_1.py
@@ -3,14 +3,25 @@ class Parser:
     def __init__(self):
         print("init")
 
-    def parseQuery(self, queryString): 
+    def parseQuery(query_string): 
         '''
         Takes a query string as input
         and parses it into a format that 
         the datastore component can understand.
         Returns string that is in valid format to get information from the firebase
         '''
-        print('parseQuery')
+        #define parsers for words and characters
+        word_parser = Word(alphas)
+        char_parser = Word(printables, excludeChars=" ")
+
+        #define a parser for one or more words or characters
+        sentence_parser = ZeroOrMore(word_parser | char_parser)
+
+        #parse the query
+        parsed_query = sentence_parser.parseString(query_string)
+
+
+        return parsed_query
 
     def executeQuery(self, parsedQuery): 
         '''
