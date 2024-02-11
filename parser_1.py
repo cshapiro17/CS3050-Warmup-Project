f0e7ad898275dc18a769a6ac920d409c4c665a29
liam <lcasey1@uvm.edu>
2024-02-11 15:46:37 -0500

    working with parser.py

--- parser_1.py
+++ parser_1.py
@@ -11,18 +11,63 @@ class Parser:
         the datastore component can understand.
         Returns string that is in valid format to get information from the firebase
         '''
-        #define parsers for words and characters
-        word_parser = Word(alphas)
-        char_parser = Word(printables, excludeChars=" ")
-        #double quote parser 
-        quoted_parser = quotedString.setParseAction(lambda t: t[0][1:-1])
+        # #define parsers for words and characters
+        # word_parser = Word(alphas)
+        # char_parser = Word(printables, excludeChars=" ")
+        # #double quote parser 
+        
 
-        #define a parser for one or more words or characters
-        sentence_parser = ZeroOrMore(word_parser | char_parser | quoted_parser)
-        #parse the query
-        parsed_query = sentence_parser.parseString(query_string)
+        # #define a parser for one or more words or characters
+        # sentence_parser = ZeroOrMore(word_parser | char_parser )
+        # #parse the query
+        # parsed_query = sentence_parser.parseString(query_string)
+
+        # return parsed_query
+        word_parser = Word(alphas)
+        char_parser = Word(printables, excludeChars='" ')
+
+        # Define a parser for quoted strings
+        quoted_parser = '"' + Word(printables, excludeChars='"') + '"'
+
+        # Define a parser for one or more words, characters, or quoted strings
+        sentence_parser = word_parser | char_parser
+
+        # Parse the query
+        parsed_query = []
+
+        # Flag to indicate if the parser is currently within a quoted string
+        in_quoted_string = False
+
+        # Temporary buffer to accumulate characters in a quoted string
+        quoted_buffer = ""
+
+        # Iterate through the query string
+        for char in query_string:
+            if char == '"':
+                if in_quoted_string:
+                    # End of quoted string, add to parsed_query
+                    parsed_query.append(quoted_buffer)
+                    quoted_buffer = ""
+                    in_quoted_string = False
+                else:
+                    # Start of quoted string
+                    in_quoted_string = True
+            elif in_quoted_string:
+                # Inside a quoted string, use quoted parser
+                parsed_query.append(quoted_parser.parseString(query_string).asList()[0])
+            else:
+                # Outside quoted string, use regular parsers
+                parsed_query.append(sentence_parser.parseString(char).asList()[0])
+
+        # If there's anything left in the quoted_buffer, add it to parsed_query
+        if quoted_buffer:
+            parsed_query.append(quoted_buffer)
+
+        # Join the parsed tokens into a single string
+        parsed_query = ' '.join(parsed_query)
 
         return parsed_query
+
 
     def execute_query(self, parsed_query): 
         '''
